27e1a51067fe29a580004ef96f27b8d613baff74
Nelson Jumbo <njumbo@thoughtworks.com>
2020-12-17 09:43:24 +0100

    Extract contact row to a different component

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { useCallback, useState, useMemo } from 'react';
+import {useCallback, useState, useMemo} from 'react';
 import Table from 'react-bootstrap/Table';
-import { useDispatch, useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
-import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core';
+import {AppBar, Button, IconButton, Toolbar, Typography} from '@material-ui/core';
 import StarIcon from '@material-ui/icons/Star';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 
-import { Contact } from '../../models/Contact';
-import * as contact from '../store/actions';
+import * as contactActions from '../store/actions';
 import * as fromContacts from '../store/selectors';
-import { Search } from './Search';
+import {Search} from './Search';
+import {Contact} from "../../models/Contact";
 
 const REGEXP_STR = '^[0-9]{9}$';
 
@@ -53,6 +53,36 @@ function useChuckNorris(): [string, (event: React.MouseEvent<HTMLButtonElement>)
     return [joke, onClick];
 }
 
+type ContactRowProps = {
+    contact: Contact
+}
+
+const ContactRow = ({contact}: ContactRowProps) => {
+    const dispatch = useDispatch()
+
+    const setAsFav = useCallback(
+        (favContact: Contact) => {
+            favContact.isFavorite = !favContact.isFavorite;
+            dispatch(contactActions.update(favContact));
+        },
+        []
+    );
+
+    return <tr className={`contact ${contact.isFavorite ? 'favorite' : ''}`} key={contact.phone}>
+        <td role="name">{contact.name}</td>
+        <td role="phone">{contact.phone}</td>
+        <td>
+            <IconButton
+                aria-label="Fav"
+                onClick={() => setAsFav(contact)}
+                color={contact.isFavorite ? 'primary' : 'default'}
+            >
+                <StarIcon/>
+            </IconButton>
+        </td>
+    </tr>
+}
+
 export const ContactList = () => {
     const contacts = useSelector(fromContacts.contactList);
     const dispatch = useDispatch();
@@ -62,14 +92,6 @@ export const ContactList = () => {
     const [joke, getJoke] = useChuckNorris();
     const classes = useStyles();
 
-    const setAsFav = useCallback(
-        (favContact: Contact) => {
-            favContact.isFavorite = !favContact.isFavorite;
-            dispatch(contact.update(favContact));
-        },
-        [contacts]
-    );
-
     return (
         <>
             <AppBar position="static">
@@ -77,48 +99,36 @@ export const ContactList = () => {
                     <Typography className={classes.title} variant="h6" noWrap>
                         {'super contact list!'.toUpperCase()}
                     </Typography>
-                    <Search searchValue={searchValue} onChange={setSearchValue} />
+                    <Search searchValue={searchValue} onChange={setSearchValue}/>
                 </Toolbar>
             </AppBar>
             <Table bordered hover>
                 <thead>
-                    <tr>
-                        <td>Nombre</td>
-                        <td>Número</td>
-                        <td>Fav!</td>
-                    </tr>
+                <tr>
+                    <td>Nombre</td>
+                    <td>Número</td>
+                    <td>Fav!</td>
+                </tr>
                 </thead>
                 <tbody>
-                    {contacts.filter((contact) => contact.phone.includes(searchValue) || contact.name.includes(searchValue)).map((contact) => {
-                        return (
-                            <tr className={`contact ${contact.isFavorite ? 'favorite' : ''}`} key={contact.phone}>
-                                <td role="name">{contact.name}</td>
-                                <td role="phone">{contact.phone}</td>
-                                <td>
-                                    <IconButton
-                                        aria-label="Fav"
-                                        onClick={() => setAsFav(contact)}
-                                        color={contact.isFavorite ? 'primary' : 'default'}
-                                    >
-                                        <StarIcon />
-                                    </IconButton>
-                                </td>
-                            </tr>
-                        );
-                    })}
+                {
+                    contacts
+                    .filter((contact) => contact.phone.includes(searchValue) || contact.name.includes(searchValue))
+                    .map((contact) => <ContactRow contact={contact} key={contact.phone}/>)
+                }
                 </tbody>
             </Table>
-            <form onSubmit={() => dispatch(contact.add(new Contact(inputPhone, inputName)))}>
+            <form onSubmit={() => dispatch(contactActions.add(new Contact(inputPhone, inputName)))}>
                 <label htmlFor="input-name">Nombre</label>
-                <input type="text" id="input-name" onChange={(event: any) => setInputName(event.target.value)} />
+                <input type="text" id="input-name" onChange={(event: any) => setInputName(event.target.value)}/>
                 <label htmlFor="input-phone">Número</label>
-                <input type="text" id="input-phone" onChange={onChangePhone} pattern={REGEXP_STR} />
+                <input type="text" id="input-phone" onChange={onChangePhone} pattern={REGEXP_STR}/>
 
                 <Button variant="contained" disabled={isDisabled} type="submit">
                     Añade nuevo contacto
                 </Button>
             </form>
-            <br />
+            <br/>
 
             <Button variant="contained" color="primary" onClick={getJoke}>
                 Chucknorrisame!
